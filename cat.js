2911568a8d4b6ae70df80faa43a61de35aa1becc
Jackson Popkin + Ricardo Quinones <jdpopkin@gmail.com>
2013-09-20 12:02:11 -0400

    First commit

---
+++ cat.js
@@ -0,0 +1,11 @@
+function Cat(name, owner) {
+  this.name = name;
+  this.owner = owner;
+}
+
+Cat.prototype.cuteStatement = function() {
+  console.log(this.owner + " loves " + this.name);
+}
+
+cat = new Cat("Sennacy", "Jonathan");
+cat.cuteStatement;
